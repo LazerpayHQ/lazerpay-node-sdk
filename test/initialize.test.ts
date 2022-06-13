c1216728c377301527e8e6a232d96e7bdecab5df
iamnotstatic <staticdev20046@gmail.com>
2022-06-13 17:19:14 +0400

    Added unit test for crypto swap

--- test/initialize.test.ts
+++ test/initialize.test.ts
@@ -75,4 +75,34 @@ describe('#Transaction module', () => {
       console.log(e);
     }
   });
+
+  it('should swap crypto for another crypto', async () => {
+    const payload = {
+      amount: 1,
+      fromCoin: 'BUSD',
+      toCoin: 'USDT',
+      blockchain: 'Binance Smart Chain',
+    };
+    try {
+      const response = await lazer.Swap.cryptoSwap(payload);
+      expect(typeof response).toBe('object');
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  it('should get crypto swap amount out', async () => {
+    const payload = {
+      amount: 1,
+      fromCoin: 'BUSD',
+      toCoin: 'USDT',
+      blockchain: 'Binance Smart Chain',
+    };
+    try {
+      const response = await lazer.Swap.getCryptoSwapAmountOut(payload);
+      expect(typeof response).toBe('object');
+    } catch (e) {
+      console.log(e);
+    }
+  });
 });
