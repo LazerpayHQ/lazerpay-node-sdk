708fc21c799329352aa583f874bb63f86b46d910
iamnotstatic <staticdev20046@gmail.com>
2022-06-13 16:06:59 +0400

    Added crypto swap API URL constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,5 @@ export const API_URL_CONFIRM_TRANSACTION = `${API_URL}/transaction/verify`;
 export const API_URL_GET_ACCEPTED_COINS = `${API_URL}/coins`;
 export const API_URL_TRANSFER_FUNDS = `${API_URL}/transfer`;
 export const API_URL_PAYMENT_LINK = `${API_URL}/payment-links`;
+export const API_URL_CRYPTO_SWAP = `${API_URL}/swap/crypto`;
+export const API_URL_GET_CRYPTO_AMOUNT_OUT = `${API_URL}/swap/crypto/amount-out`;
