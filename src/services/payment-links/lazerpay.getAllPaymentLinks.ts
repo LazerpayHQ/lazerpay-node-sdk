3cd5dc646c34232a61ae337a555df11f014b9621
iamnotstatic <staticdev20046@gmail.com>
2022-07-10 03:39:18 +0400

    Removed redundant Request header set

--- src/services/payment-links/lazerpay.getAllPaymentLinks.ts
+++ src/services/payment-links/lazerpay.getAllPaymentLinks.ts
@@ -1,16 +1,14 @@
-import { LazerApi, setapiPubKey, setApiSecKey } from '../../utils/api';
+import { LazerApi, setApiSecKey } from '../../utils/api';
 import { API_URL_PAYMENT_LINK } from '../../utils/constants';
 
 type PaymentLinkData = {
-  apiPubKey: string;
   apiSecKey: string;
 };
 
 export default async function(args: PaymentLinkData) {
-  const { apiPubKey, apiSecKey } = args;
+  const { apiSecKey } = args;
 
   try {
-    await setapiPubKey(apiPubKey);
     await setApiSecKey(apiSecKey);
     const response = await LazerApi.get(API_URL_PAYMENT_LINK);
 
--- src/services/payment-links/lazerpay.getPaymentLink.ts
+++ src/services/payment-links/lazerpay.getPaymentLink.ts
@@ -1,17 +1,15 @@
-import { LazerApi, setapiPubKey, setApiSecKey } from '../../utils/api';
+import { LazerApi, setApiSecKey } from '../../utils/api';
 import { API_URL_PAYMENT_LINK } from '../../utils/constants';
 
 type PaymentLinkData = {
   identifier: string;
-  apiPubKey: string;
   apiSecKey: string;
 };
 
 export default async function(args: PaymentLinkData) {
-  const { apiPubKey, apiSecKey, identifier } = args;
+  const { apiSecKey, identifier } = args;
 
   try {
-    await setapiPubKey(apiPubKey);
     await setApiSecKey(apiSecKey);
     const response = await LazerApi.get(
       `${API_URL_PAYMENT_LINK}/${identifier}`
