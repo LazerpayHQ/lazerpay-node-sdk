1a276d96c3566e9a5fd2bf243d74739b61a11a77
iamnotstatic <staticdev20046@gmail.com>
2023-02-12 12:15:56 +0400

    Added onramp api url reference

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,8 +3,12 @@ export const API_URL = 'https://api.lazerpay.engineering/api/v1';
 export const API_URL_INIT_TRANSACTION = `${API_URL}/transaction/initialize`;
 export const API_URL_CONFIRM_TRANSACTION = `${API_URL}/transaction/verify`;
 export const API_URL_GET_ACCEPTED_COINS = `${API_URL}/coins`;
-export const API_URL_TRANSFER_FUNDS = `${API_URL}/transfer`;
+export const API_URL_TRANSFER_FUNDS = `${API_URL}/crypto/payouts/initiate`;
 export const API_URL_PAYMENT_LINK = `${API_URL}/payment-links`;
 export const API_URL_CRYPTO_SWAP = `${API_URL}/swap/crypto`;
 export const API_URL_GET_CRYPTO_AMOUNT_OUT = `${API_URL}/swap/crypto/amount-out`;
 export const API_URL_GET_WALLET_BALANCE = `${API_URL}/wallet/balance`;
+
+export const API_URL_GET_ONRAMP_RATE = `${API_URL}/bank/funding/rate`;
+export const API_URL_INITIATE_ONRAMP = `${API_URL}/bank/funding/initiate`;
+export const API_URL_GET_ONRAMP_ACCOUNTS = `${API_URL}/bank/funding/accounts`;
